cd805613d347c6f2c9b31035650a429b13ea4207
Vishal Makode <vishal.makode@q-bot.co>
2020-09-06 20:03:59 +0100

    added a new tf broadcaster

--- src/drone_control/src/marker_frame_broadcaster.py
+++ src/drone_control/src/marker_frame_broadcaster.py
@@ -1,54 +1,44 @@
 #!/usr/bin/env python
-
-from tf import TransformBroadcaster
 import rospy
-from rospy import Time
+import tf2_ros
+import tf2_msgs.msg
+import geometry_msgs.msg
 from drone_control.msg import MarkerPosition
 
 
-class MarkerFrameBroadcast:
+class FixedTFBroadcaster:
 
-    def __init__(self, broadcast_handle):
-        self.b = broadcast_handle
-        self.translation = (1.0, 1.0, 0.0)
-        self.rotation = (0.0, 0.0, 0.0, 1.0)
-        self._tag_location_sub = rospy.Subscriber('/location/tag', MarkerPosition, queue_size=5,
-                                                  callback=self.broadcast)
+    def __init__(self):
+        self.pub_tf = rospy.Publisher("/tf", tf2_msgs.msg.TFMessage, queue_size=1)
+        self.x = 0
+        self._tag_detect_sub = rospy.Subscriber('/location/tag', MarkerPosition, queue_size=5, callback=self.display_frame)
 
-    def broadcast(self, message):
+    def display_frame(self, message):
+        # Run this loop at about 10Hz
         if message.isTagVisible:
-            message = MarkerPosition()
-            self.translation = (message.position.position.x, message.position.position.y, message.position.position.z)
-            self.rotation = (message.position.orientation.x, message.position.orientation.y, message.position.orientation.z,
-                             message.position.orientation.w)
-            self.b.sendTransform(self.translation, self.rotation, Time.now(), '/word', 'marker_frame')
-        else:
-            self.b.sendTransform(self.translation, self.rotation, Time.now(), '/world',
-                                 'marker_frame')
+            rospy.sleep(0.1)
+            self.x += 1
+            if self.x > 3:
+                self.x = 0
+            t = geometry_msgs.msg.TransformStamped()
+            t.header.frame_id = "world"
+            t.header.stamp = rospy.Time.now()
+            t.child_frame_id = "carrot1"
+            t.transform.translation.x = 0.0
+            t.transform.translation.y = self.x
+            t.transform.translation.z = 0.0
 
+            t.transform.rotation.x = 0.0
+            t.transform.rotation.y = 0.0
+            t.transform.rotation.z = 0.0
+            t.transform.rotation.w = 1.0
 
-def main():
-    translation = (0.0, 0.0, 0.0)
-    rotation = (0.0, 0.0, 0.0, 1.0)
-    rate = rospy.Rate(5)  # 5hz
-
-    x, y = 0.0, 0.0
-
-    while not rospy.is_shutdown():
-        if x >= 2:
-            x, y = 0.0, 0.0
-
-        x += 0.1
-        y += 0.1
-
-        translation = (x, y, 0.0)
-
-        #b.sendTransform(translation, rotation, Time.now(), '/world', 'marker_frame')
-        rate.sleep()
-
+            tfm = tf2_msgs.msg.TFMessage([t])
+            self.pub_tf.publish(tfm)
 
 if __name__ == '__main__':
-    rospy.init_node('marker_frame_broadcaster')
-    frame_broadcast = MarkerFrameBroadcast(broadcast_handle=TransformBroadcaster())
+
+    rospy.init_node('fixed_tf2_broadcaster')
+    tfb = FixedTFBroadcaster()
 
     rospy.spin()
